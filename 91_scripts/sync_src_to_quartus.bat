48bf456de8c06dc42cb61602180a528302caf424
Nguyễn Duy Ngọc <duyngoc131004@gmail.com>
2025-09-19 20:55:13 +0700

    Modify Pipelined CPU into Single-cycle CPU: lint checked, quartus synthesized, diagrams of datapath and controller updated, not yet tested

--- 91_scripts/sync_src_to_quartus.bat
+++ 91_scripts/sync_src_to_quartus.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Syncing modified files from OneDrive to Quartus folder...
-robocopy "C:\Users\NCPC\OneDrive\BKLearning\CapstoneProject1\00_src" "C:\CapstoneProject1_Quartus\top\00_src" /MIR /XD .git __pycache__
+robocopy "C:\Users\NCPC\OneDrive\BKLearning\RISC-V_Single-cycle_Processor\00_src" "C:\RISC-V_Single-cycle_Processor_Quartus\top\00_src" /MIR /XD .git __pycache__
 echo Done.
 pause
